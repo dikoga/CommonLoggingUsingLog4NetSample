570f951404a6e66f2c5c4fa2b7f0b64ef3d2f3e4
dikoga <dikoga@gmail.com>
2016-03-01 08:30:01 -0500

    Initial Commit

---
+++ CommonLoggingUsingLog4NetSample/CommonLoggingUsingLog4NetSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using Common.Logging;
+
+namespace CommonLoggingUsingLog4NetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = LogManager.GetLogger(typeof(Program));
+            log.Debug("test...");
+        }
+    }
+}
